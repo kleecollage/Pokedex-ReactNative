e9ea8a1d9d9bd2753448bf3ba6738ed1f6df3a64
Klee Collage <klee.collage@gmail.com>
2024-10-02 02:12:42 -0500

    Project Structure
    Assets included

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './gesture-handler';
+import * as React from 'react'; 
 import { Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { StackNavigator } from './presentation/navigator/StackNavigator';
 
 export const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
   )
 }
---
+++ src/presentation/navigator/StackNavigator.tsx
@@ -0,0 +1,16 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from '../screens/home/HomeScreen';
+import { PokemonScreen } from '../screens/pokemon/PokemonScreen';
+import { SearchScreen } from '../screens/search/SearchScreen';
+
+const Stack = createStackNavigator();
+
+export const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Pokemon" component={PokemonScreen} />
+      <Stack.Screen name="Search" component={SearchScreen} />
+    </Stack.Navigator>
+  );
+}
