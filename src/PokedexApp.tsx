b2284b97f463ec65535f105d68bcdabb6e5fae8c
Klee Collage <klee.collage@gmail.com>
2024-10-02 02:58:58 -0500

    React Native Paper
    react-native-vector-icons ready

--- src/PokedexApp.tsx
+++ src/PokedexApp.tsx
@@ -2,11 +2,17 @@ import './gesture-handler';
 import * as React from 'react'; 
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './presentation/navigator/StackNavigator';
+import { PaperProvider } from 'react-native-paper';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export const PokedexApp = () => {
   return (
-    <NavigationContainer>
-      <StackNavigator />
-    </NavigationContainer>
+    <PaperProvider settings={{
+      icon: props => <Ionicons {...props} />,
+    }}>
+      <NavigationContainer>
+        <StackNavigator />
+      </NavigationContainer>
+    </PaperProvider>
   )
 }
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,9 +1,14 @@
 import { Text, View } from 'react-native'
+import { Button } from 'react-native-paper'
 
 export const HomeScreen = () => {
+
   return (
     <View>
       <Text>HomeScreen</Text>
+       <Button icon="rocket" mode="contained" onPress={() => console.log('Pressed')}>
+          Press me
+        </Button>
     </View>
   )
 }
