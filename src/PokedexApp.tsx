6411f83bd29be1cfaed233c6825584826868ce13
Klee Collage <klee.collage@gmail.com>
2024-10-02 12:58:58 -0500

    Interfaces | Mappers | Entities
    data fetched with axios, base API in api folder

--- src/PokedexApp.tsx
+++ src/PokedexApp.tsx
@@ -1,15 +1,17 @@
 import './gesture-handler';
 import * as React from 'react'; 
-import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './presentation/navigator/StackNavigator';
-import { PaperProvider } from 'react-native-paper';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import { ThemeContextProvider } from './presentation/context/ThemeContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export const PokedexApp = () => {
   return (
-    <ThemeContextProvider>
-      <StackNavigator />
-    </ThemeContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeContextProvider>
+        <StackNavigator />
+      </ThemeContextProvider>
+    </QueryClientProvider>
   )
 }
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,14 +1,28 @@
-import { View } from 'react-native'
+import { ActivityIndicator, View } from 'react-native'
 import { Button, Text } from 'react-native-paper'
+import { getPokemons } from '../../../actions/pokemons'
+import { useQuery } from '@tanstack/react-query'
 
 export const HomeScreen = () => {
+
+  const { isLoading, data = []} = useQuery({
+    queryKey: ['pokemons'], // guarda informacion en cache
+    queryFn: () => getPokemons(0),
+    staleTime: 1000 * 60 * 60, // 60 mins
+  })
 
   return (
     <View>
       <Text variant='headlineMedium'>Home Screen</Text>
-       <Button icon="rocket" mode="contained" onPress={() => console.log('Pressed')}>
-          Press me
+       
+      { isLoading ? (
+        <ActivityIndicator /> 
+        ) : (
+        <Button icon="rocket" mode="contained" onPress={() => console.log('Pressed')}>
+          Press Me
         </Button>
+        )}
+       
     </View>
   )
 }
